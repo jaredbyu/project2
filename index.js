00535ebc91ac552cbaf9656db7d12c0c6e693005
Jaredbyu <bro15032@byui.edu>
2017-11-17 15:45:10 -0700

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/home', function(request, response) {
   response.render('pages/index')
 });
 
