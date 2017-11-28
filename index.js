228c8ef0ba43169a0ffb7a6c0f97f25f47638a0a
Jaredbyu <bro15032@byui.edu>
2017-11-28 14:26:25 -0700

    update

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ app.get('/', function(request, response) {
 
 app.get('/inventory', function(request, response) {
     
-   
+  getPerson(request, response);
     
-  response.render('pages/inventory')
+ // response.render('pages/inventory')
 });
 
 app.get('/inventory/colors', function(request, response) {
@@ -50,6 +50,65 @@ app.listen(app.get('port'), function() {
 });
 
 
+function getPerson(request, response) {
+	// First get the person's id
+	var id = request.query.id;
+
+	// TODO: It would be nice to check here for a valid id before continuing on...
+
+	// use a helper function to query the DB, and provide a callback for when it's done
+	getPersonFromDb(id, function(error, result) {
+		// This is the callback function that will be called when the DB is done.
+		// The job here is just to send it back.
+
+		// Make sure we got a row with the person, then prepare JSON to send back
+		if (error || result == null || result.length != 1) {
+			response.status(500).json({success: false, data: error});
+		} else {
+			var person = result[0];
+			response.status(200).json(result[0]);
+		}
+	});
+}
+
+function getPersonFromDb(id, callback) {
+	console.log("Getting person from DB with id: " + id);
+
+	var client = new pg.Client(connectionString);
+
+	client.connect(function(err) {
+		if (err) {
+			console.log("Error connecting to DB: ")
+			console.log(err);
+			callback(err, null);
+		}
+
+		var sql = "SELECT id, first, last, birthdate FROM person WHERE id = $1::int";
+		var params = [id];
+
+		var query = client.query(sql, params, function(err, result) {
+			// we are now done getting the data from the DB, disconnect the client
+			client.end(function(err) {
+				if (err) throw err;
+			});
+
+			if (err) {
+				console.log("Error in query: ")
+				console.log(err);
+				callback(err, null);
+			}
+
+			console.log("Found result: " + JSON.stringify(result.rows));
+
+			// call whatever function the person that called us wanted, giving it
+			// the results that we have been compiling
+			callback(null, result.rows);
+		});
+	});
+
+} // end of getPersonFromDb
+                   
+                   
 
     
 
