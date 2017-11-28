16ff03aa0ec3fef7b263c8acc8d2771e103c3ac0
Jaredbyu <bro15032@byui.edu>
2017-11-28 14:47:16 -0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+var router = app.Router();
 
 
 var pg = require("pg"); // This is the postgres database connection module.
@@ -50,61 +51,25 @@ app.listen(app.get('port'), function() {
 });
 
 
-function getPerson(request, response) {
-	
+router.get('/inventory', function(req, res, next) {
+  pg.connect(connectionString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('SELECT * FROM inventory', function(err, result) {
+      done();
+      if (err) {
+        return console.error('error running query', err);
+      }
+      res.send(result);
+    });
+  });
+});
 
-	// use a helper function to query the DB, and provide a callback for when it's done
-	getPersonFromDb(function(error, result) {
-		// This is the callback function that will be called when the DB is done.
-		// The job here is just to send it back.
 
-		// Make sure we got a row with the person, then prepare JSON to send back
-		if (error || result == null || result.length != 1) {
-			response.status(500).json({success: false, data: error});
-		} else {
-			var person = result[0];
-			response.status(200).json(result[0]);
-		}
-	});
-}
 
-function getPersonFromDb(callback) {
-    var id = 1;
-	console.log("Getting person from DB with id: " + id);
-
-	var client = new pg.Client(connectionString);
-
-	client.connect(function(err) {
-		if (err) {
-			console.log("Error connecting to DB: ")
-			console.log(err);
-			callback(err, null);
-		}
-
-		var sql = "SELECT * FROM INVENTORY";
-		//var params = [id];
-
-		var query = client.query(sql, function(err, result) {
-			// we are now done getting the data from the DB, disconnect the client
-			client.end(function(err) {
-				if (err) throw err;
-			});
-
-			if (err) {
-				console.log("Error in query: ")
-				console.log(err);
-				callback(err, null);
-			}
-
-			console.log("Found result: " + JSON.stringify(result.rows));
-
-			// call whatever function the person that called us wanted, giving it
-			// the results that we have been compiling
-			callback(null, result.rows);
-		});
-	});
-
-} // end of getPersonFromDb
+module.exports = router;
                    
                    
 
