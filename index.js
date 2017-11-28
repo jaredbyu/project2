d6bca229701740c26d7880874d2d83f6e2d89efc
Jaredbyu <bro15032@byui.edu>
2017-11-28 16:45:11 -0700

    added twiter api

--- index.js
+++ index.js
@@ -13,55 +13,43 @@ app.set('view engine', 'ejs');
 
 
 
-var T = new Twit({
+var TwitterKeys = new Twit({
         consumer_key:           'wv3ds5CXa6rl5PmZmDLEUGe9p',
         consumer_secret:        'VtPiQ6xJ9xU9u3X86GR3YaWhAI9roHGKtEXYm7TAU58Vx9xUF4',
-        access_token:           'V2QtXcLNeKggKjLYVa5yCA29ZvrqKqECQWS8NR2',
+        access_token:           '1525279621-V2QtXcLNeKggKjLYVa5yCA29ZvrqKqECQWS8NR2',
         access_token_secret:    'cPnRK39salFSWjhtTwu502kLT5oD7Irzmj7tDpXZGDa82'
 });
 
 
-app.post('/getPaperTweets', getPaperTweets);
+function searchTwitter(request, response) {
 
-
-function getPaperTweets(request, response) {
-    var searchParam = 'paper';
-
-    console.log("Searching for " + "\"" + searchParam + "\"");
-
-    var params = {
-        q: searchParam,
+    var parameters = {
+        q: 'paper',
         lang: 'en',
         locale: 'en',
-        count: 1
+        count: 100
     }
 
-    T.get('search/tweets', params, function(err, data) {
-        
+    TwitterKeys.get('search/tweets', parameters, function(err, data) {
         
         if (err){
-            console.log("Error");
+            console.log(err);
         }
-        
         
         var tweets = data.statuses;
         
         for (var i = 0; i < tweets.length; i++) {
-            //console.log(tweets[i].user.screen_name + ": " + tweets[i].text);
-            //console.log(tweets[i]); // displays the whole object returned from Twitter
-            var result = {tweets: tweets[i].text,
+            var message = {tweets: tweets[i].text,
                         user: tweets[i].user.screen_name};
         }
-        response.json(result);
-        //console.log(result);
-        
+        response.json(message);
     });
-
 }
 
 
 
 app.get('/', function(request, response) {
+    app.post('/searchTweets', searchTwitter);
   response.render('pages/index')
 });
 
