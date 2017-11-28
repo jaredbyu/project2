ee050350318453b877747d418565d6d1739692a9
Jaredbyu <bro15032@byui.edu>
2017-11-28 14:35:16 -0700

    update

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function getPersonFromDb(callback) {
 			callback(err, null);
 		}
 
-		var sql = "SELECT id, first, last, birthdate FROM person WHERE id = $1::int";
+		var sql = "SELECT * FROM INVENTORY";
 		var params = [id];
 
 		var query = client.query(sql, params, function(err, result) {
